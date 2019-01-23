13aa1e5398da32d6433cdda4e645871125e8af24
DorinDavidov <dorinyme@gmail.com>
2019-01-22 21:22:24 -0500

    drill3

---
+++ ex1_drill3.py
@@ -0,0 +1,9 @@
+#print ("Hello World")
+#print ("Hello Again")
+#print ("I like typing this")
+#print ("This is fun")
+print ("Yay! Printing")
+#print ("I'd much rather you 'not'")
+#print ("I 'said' do not touch this.")
+
+#The # sign cancel the command and make a not or an explnation.
